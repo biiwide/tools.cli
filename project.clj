30f8fda549c25bcf7924c5a8a8e9a11d21215abf
Ted Cushman <biiwide@users.noreply.github.com>
2018-02-21 17:06:55 -0600

    Increment to snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject biiwide/tools.cli "0.3.9"
+(defproject biiwide/tools.cli "0.3.10-SNAPSHOT"
   :description "Command line arguments library."
   :parent [org.clojure/pom.contrib "0.1.2"]
   :url "https://github.com/biiwide/tools.cli"

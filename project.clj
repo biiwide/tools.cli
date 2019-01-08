22e5c8f6d661371cad7d062052170bf48528656b
Ted Cushman <biiwide@users.noreply.github.com>
2019-01-08 14:54:27 -0600

    Overriding :release-tasks

--- project.clj
+++ project.clj
@@ -1,20 +1,21 @@
 (defproject biiwide/tools.cli "0.3.10-SNAPSHOT"
+
   :description "Command line arguments library."
   :parent [org.clojure/pom.contrib "0.1.2"]
   :url "https://github.com/biiwide/tools.cli"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+
+  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
 
              ;; Local CLJS development; not in pom.xml
              :dev {:dependencies [[org.clojure/clojurescript "0.0-2080" :scope "test"]]
@@ -31,6 +32,18 @@
                                                             :runner
                                                             "target/cli_test.js"]}}}}
 
-  :aliases {"test-all" ["with-profile" "test,1.2:test,1.3:test,1.4:test,1.5:test,1.6:test,1.7:test,1.8:test,1.9" "test"]
-            "check-all" ["with-profile" "1.2:1.3:1.4:1.5:1.6:1.7:1.8:1.9" "check"]}
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+
+  :aliases {"test-all" ["with-profile" "test,1.6:test,1.7:test,1.8:test,1.9" "test"]
+            "check-all" ["with-profile" "1.6:1.7:1.8:1.9" "check"]}
+
   :min-lein-version "2.0.0")
